8cc1bde05c36b6d2f4c5a5311df64ec3d4d8aa60
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-07 20:46:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,8 +14,8 @@ class School
     @roster[grade]
   end
   def sort
-    @roster.each do |grade,student|
-      student.sort!
+    @roster=@roster.each do |grade,student|
+      student.sort
     end
   end
 end
